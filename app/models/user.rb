9cd20a41b1828fa349fd3b1fdfd93a03757c80ae
ThomasVadis <o.thomasvadis@gmail.com>
2024-06-06 20:00:45 -0400

    encrpyted password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,13 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 8, maximum: 64 }
   validates_uniqueness_of :username
 
+  validates :password, presence: true, length: { minimum: 8, maximum: 64 }
+  before_create :hash_password
+  private
+  def hash_password
+    self.password = BCrypt::Password.create(self.password)
+  end
+
+
 
 end
